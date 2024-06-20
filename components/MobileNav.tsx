dc8884f760ae64ddc3248acec0ce9ee9b935a506
Confidence Emonena <confidinho@yahoo.com>
2024-06-20 13:35:43 +0100

    mobile nav and sidebar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import sidebarLinks from "@/constants";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="w-fulll max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={36}
+            height={36}
+            alt="hamburger menu"
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link
+            href="/"
+            className="cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Yoom logo"
+            />
+            <h1 className="text-[26px] font-extrabold text-white">Yoom</h1>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("flex gap-3 p-2 rounded-sm w-full", {
+                          "bg-blue-1": isActive,
+                        })}
+                      >
+                        <Image
+                          src={item.imgUrl}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+                        <p
+                          className={cn(
+                            "text-[16px] font-semibold text-black-2",
+                            {
+                              "text-white": isActive,
+                            }
+                          )}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
